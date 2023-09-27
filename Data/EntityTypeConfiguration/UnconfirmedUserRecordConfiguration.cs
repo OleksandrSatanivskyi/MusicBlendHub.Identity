c249515c6d5919b128c654f905f0dd0ebbd8149a
Sasha <oleksandrsy@gmail.com>
2023-09-27 21:22:48 +0300

    Create UnconfirmedUserRecordConfiguration class

---
+++ Data/EntityTypeConfiguration/UnconfirmedUserRecordConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using MusicBlendHub.Identity.Models;
+
+namespace MusicBlendHub.Identity.Data.EntityTypeConfiguration
+{
+    public class UnconfirmedUserRecordConfiguration : IEntityTypeConfiguration<UnconfirmedUserRecord>
+    {
+        public void Configure(EntityTypeBuilder<UnconfirmedUserRecord> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Id).IsUnique();
+            builder.Property(x => x.Name).HasMaxLength(16);
+            builder.Property(x => x.Password).HasMaxLength(256);
+        }
+    }
+}
