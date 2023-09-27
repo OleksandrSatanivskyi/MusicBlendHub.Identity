6b833f5cb901c976acf273fe0bc000006383488e
Sasha <oleksandrsy@gmail.com>
2023-09-27 21:32:06 +0300

    Create DependencyInjection class

---
+++ Data/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicBlendHub.Identity.Data.DbContexts;
+
+namespace MusicBlendHub.Identity.Data
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["SqlServerDbConnection"];
+            services.AddDbContext<SqlServerDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IAuthDbContext>(provider => provider.GetService<SqlServerDbContext>());
+
+            return services;
+        }
+    }
+}
