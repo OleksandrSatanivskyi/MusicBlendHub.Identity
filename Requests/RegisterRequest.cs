c4d5a00774e4efb95e7670225baa761a7ffc066d
Sasha <oleksandrsy@gmail.com>
2023-09-27 21:26:19 +0300

    Create RegisterRequest class

---
+++ Requests/RegisterRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Pastebin.Identity.Requests
+{
+    public class RegisterRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
