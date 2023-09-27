25b7ce624dd47b3229d749822534e14ec9addb9f
Sasha <oleksandrsy@gmail.com>
2023-09-27 21:15:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace MusicBlendHub.Identity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder.Services, builder.Configuration);
+
+            var app = builder.Build();
+            ConfigureApp(app);
+
+
+            app.Run();
+
+        }
+
+        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddPersistence(configuration);
+            services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyPolicy",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+        }
+
+        private static void ConfigureApp(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+            app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAnyPolicy"); //change in future
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
