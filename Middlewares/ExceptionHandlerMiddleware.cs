1981c3de3537429cf0a306176ef77bc8aea42549
Sasha <oleksandrsy@gmail.com>
2023-09-27 21:24:45 +0300

    Add ExceptionHandlerMiddleware class

---
+++ Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace MusicBlendHub.Identity.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        public readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            var errorResponse = new { Message = exception.Message};
+
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(errorResponse));
+        }
+    }
+}
