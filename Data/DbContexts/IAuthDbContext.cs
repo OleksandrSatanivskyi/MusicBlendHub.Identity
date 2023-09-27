db66d6adc6c1d450e6adf25cb116fc63a398a669
Sasha <oleksandrsy@gmail.com>
2023-09-27 21:21:42 +0300

    Create IAuthDbContext interface

---
+++ Data/DbContexts/IAuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using MusicBlendHub.Identity.Models;
+using System.Collections.Generic;
+
+namespace MusicBlendHub.Identity.Data.DbContexts
+{
+    public interface IAuthDbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<UnconfirmedUserRecord> UnconfirmedUserRecords { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken token);
+        Task<int> SaveChangesAsync();
+    }
+}
