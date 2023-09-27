51f1b32721f978a801c43436c99106559429b98e
Sasha <oleksandrsy@gmail.com>
2023-09-27 21:22:30 +0300

    Create UserConfiguration class

---
+++ Data/EntityTypeConfiguration/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using MusicBlendHub.Identity.Models;
+
+namespace MusicBlendHub.Identity.Data.EntityTypeConfiguration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Id).IsUnique();
+            builder.Property(x => x.Name).HasMaxLength(16);
+            builder.Property(x => x.Password).HasMaxLength(256);
+        }
+    }
+}
