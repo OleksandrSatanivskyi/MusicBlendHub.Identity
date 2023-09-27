774ddc2f647d8f97226009565f66a76556bad900
Sasha <oleksandrsy@gmail.com>
2023-09-27 21:23:02 +0300

    Create SqlServerDbContext class

---
+++ Data/DbContexts/SqlServerDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicBlendHub.Identity.Data.EntityTypeConfiguration;
+using MusicBlendHub.Identity.Models;
+
+namespace MusicBlendHub.Identity.Data.DbContexts
+{
+    public class SqlServerDbContext : DbContext, IAuthDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UnconfirmedUserRecord> UnconfirmedUserRecords { get; set; }
+
+        public SqlServerDbContext(DbContextOptions<SqlServerDbContext> options) : base(options) { }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new UnconfirmedUserRecordConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
