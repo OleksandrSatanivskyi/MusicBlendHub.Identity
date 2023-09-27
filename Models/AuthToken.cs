3d7d10c8267d414aa966c7093c2786e5ca32b1dd
Sasha <oleksandrsy@gmail.com>
2023-09-27 21:19:43 +0300

    Create AuthToken class

---
+++ Models/AuthToken.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicBlendHub.Identity.Models
+{
+    public class AuthToken
+    {
+        public string TokenString { get; set;}
+
+        public AuthToken(string tokenString)
+        {
+            TokenString = tokenString;
+        }
+    }
+}
